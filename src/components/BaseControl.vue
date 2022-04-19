4be90cbad88ee1d91dddceec713b2743f279b569
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-04-19 15:32:12 +0100

    CHORE :: added conmponet for control icons

---
+++ src/components/BaseControl.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="mx-1 d-block">
+    <i :class="'icofont-' + icon"></i>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+     
+    },
+  },
+};
+</script>
+
+<style scoped>
+.mx-1{
+margin: 0 7.5px;
+}
+</style>
