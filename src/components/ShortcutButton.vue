bbf4dd8126eaeb4347fccb0361fc48e92348a389
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-04-19 11:48:29 +0100

    CHORE

---
+++ src/components/ShortcutButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+button {
+  font-size: 1.25rem;
+  padding: 2px;
+  margin: 0 5px;
+  font-family: "Times New Roman", Times, serif;
+}
+
+button.bold {
+  font-weight: 300;
+  font-family: unset;
+}
+
+button.italic {
+  font-style: italic;
+}
+</style>
