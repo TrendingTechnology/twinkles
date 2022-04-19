a1fcc89baa1557b6736d857f04f8a15e83a63509
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-04-19 21:05:47 +0100

    CHORE :: setup bacic view and mounted the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Editor from '../views/Editor.vue'
+import Preview from '../views/Preview.vue'
+import Guide from '../views/Guide.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'editor',
+    component: Editor
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/guide',
+    name: 'guide',
+    component: Guide
+  },
+  {
+    path: '/preview',
+    name: 'Preview',
+    component: Preview
   }
 ]
 
---
+++ src/views/Editor.vue
@@ -0,0 +1,17 @@
+<template>
+  <textarea
+    min-height="75vh"
+    rounded="lg"
+    name=""
+    id=""
+    placeholder="write here ..."
+    v-model="rawBlob"
+  ></textarea>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
